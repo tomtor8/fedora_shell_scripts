a1ee01e17383ced9124a90d5e0c1c14c86f5f604
tomtor8 <tomas.torday@gmail.com>
2023-05-13 10:53:03 +0200

    add final formatting to check if in path

--- check_if_in_path/check_if_in_path.sh
+++ check_if_in_path/check_if_in_path.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# $1 is a function parameter passed into the function
+function divider() {
+	for ((i = 0; i < $1; i++)); do
+		echo -n "="
+	done
+}
+
 read -r -p "File name? " name
+echo
 pathfolders="$PATH"
 counter=0
 OLD_IFS="$IFS"
@@ -8,23 +16,25 @@ OLD_IFS="$IFS"
 IFS=:
 # cycle through the items in PATH variable
 # items are separated by colons
-for item in $pathfolders; do
+for pathitem in $pathfolders; do
 	# {/} prints the basename of the found file path
-	result=$(fd "$name" "$item" -x echo "{/}")
+	result=$(fd "$name" "$pathitem" -x echo "{/}")
 	if [[ "$result" != "" ]]; then
 		counter=$((counter + 1))
-		message="$counter - $name is located here: $item, with the result(s):"
+		message="$counter - $name location:"
 		# get the length of the message
 		messlen=${#message}
 		echo "$message"
 		# create a divider the length of the message
-		for ((i = 0; i < messlen; i++)); do
-			echo -n "="
-		done
+		divider "$messlen"
 		echo
+		echo "  $pathitem"
+		echo
+		echo "  File name(s):"
+		echo "  -------------"
 		IFS=$'\n'
 		for basename in $result; do
-			echo "  $basename"
+			echo "    $basename"
 			# if you want to offset the results by a width of a tab
 			# printf "\t%s\n" "$basename"
 		done
